86f6dfe92b7835def0835302230b9c6c8747a51c
adrianesara <adrianesara@hotmail.com>
2023-10-10 20:32:32 -0500

    studio completed

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -1,6 +1,25 @@
 // Initialize Variables below
+let date = "Monday 2019-03-18"
+let time = "10:05:34 AM"
+let astronautCount = 7
+let astronautStatus = "ready"
+let averageAstronautMassKg = 80.7
+let crewMassKg = astronautCount*averageAstronautMassKg
+let fuelMassKg = 760000
+let shuttleMassKg = 74842.31
+let totalMassKg = crewMassKg+fuelMassKg+shuttleMassKg
+let maximumMassLimit = 850000
+let fuelTempCelsius = -225
+let minimumFuelTemp = -300
+let maximumFuelTemp = -150
+let fuelLevel = "100%"
+let weatherStatus = "clear"
+let preparedForLiftOff = true
 
 // add logic below to verify total number of astronauts for shuttle launch does not exceed 7
+if (astronautCount<=7 && astronautStatus==="ready" && totalMassKg<maximumMassLimit && (fuelTempCelsius>=-300 || fuelTempCelsius <= -150) && fuelLevel === "100%" && weatherStatus === "clear"){
+    console.log ("All systems are a go! Initiating space shuttle sequence. \n"+ "Date: "+ date +"\nTime: " + time + "\nAstonaut Count: " + astronautCount + "\nCrew Mass: " + crewMassKg + " kg" + "\nFuel Mass: " + fuelMassKg + " kg" + "\nShuttle Mass: " + shuttleMassKg + " kg" + "\nTotal Mass: " + totalMassKg + " kg" + "\nFuel Temperature: " + fuelTempCelsius + " °C" + "\nWeather Status: " + weatherStatus + "\nHave a safe trip astronauts!" )
+}
 
 // add logic below to verify all astronauts are ready
 
