379fe3544b96cdd6312179a3eade3724c011abce
adrianesara <adrianesara@hotmail.com>
2023-11-16 20:08:45 -0600

    exercise complete

--- exceptions/exercises/divide.js
+++ exceptions/exercises/divide.js
@@ -1,3 +1,12 @@
+function divide(numerator, denominator){
+    let num = numerator/denominator
+    if (denominator === 0) {
+    throw Error("Attempted to divide by zero");
+} else {
+    return num;
+}
+}
+console.log(divide(8,2));
 // Write a function called 'divide' that takes two parameters: a numerator and a denominator.
 
 // Your function should return the result of numerator / denominator.
