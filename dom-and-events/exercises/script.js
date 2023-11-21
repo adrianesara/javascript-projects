335eb20c448a9bb41eb435b65f599d167938612c
adrianesara <adrianesara@hotmail.com>
2023-11-21 14:11:05 -0600

    exercise complete

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -5,6 +5,24 @@ function init () {
 
     // Put your code for the exercises here.
     
+    liftoffButton.addEventListener('click', event => {
+       statusReport.innerHTML = 'Houston! We have liftoff!';
+    });
+    abortMission.addEventListener("mouseover", function( event ) {
+event.target.style.backgroundColor = "red";
+});   
+abortMission.addEventListener("mouseout", function( event ) {
+    event.target.style.backgroundColor = "";       
+});
+abortMission.addEventListener('click', function (){
+    let text = "Are you sure you want to abort the mission?";
+    if (confirm(text) == true) {
+      statusReport.innerHTML = "Mission aborted! Space shuttle returning home";
+    
+    }
+    
+  });
+
 }
 
 window.addEventListener("load", init);
